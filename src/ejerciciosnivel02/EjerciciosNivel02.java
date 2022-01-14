c00ea7dbd6241e3e6e902cb705d7f74f05e81074
DAM <DAM@DESKTOP-QE3QMTS>
2022-01-14 19:20:54 +0100

    Commit inicial

---
+++ src/ejerciciosnivel02/EjerciciosNivel02.java
@@ -0,0 +1,30 @@
+package ejerciciosnivel02;
+/**
+ * @author Jorge Cisneros
+ */
+public class EjerciciosNivel02 {
+
+    public boolean esEscaleraPalabras(char[][] listaPalabras){
+    
+    
+        return true;
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char [][] listaPalabras = { 
+            {'P', 'A', 'T', 'A'}, 
+            {'P', 'A', 'T', 'O'}, 
+            {'R', 'A', 'T', 'O'}, 
+            {'R', 'A', 'M', 'O'}, 
+            {'G', 'A', 'M', 'O'}, 
+            {'G', 'A', 'T', 'O'}, 
+            {'M', 'A', 'T', 'O'}, 
+        };
+        
+    }
+    
+}
